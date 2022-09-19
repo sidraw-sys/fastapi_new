6420fbde0ebc95efae162f4b2fc14090c6da60f3
Siddharth <sidraw122@gmail.com>
2022-09-19 14:12:49 +0530

    test 1

--- app/main.py
+++ app/main.py
@@ -20,7 +20,7 @@ app.add_middleware(
 
 @app.get('/')
 def message():
-    return {"message":"Hello Visitor!!!!"}
+    return {"message":"Hello Visitor, this has been successfully deployed to production..."}
 
 app.include_router(post.router)
 app.include_router(user.router)
